39081f7c871c84a160f0ec0843837e9ef6f41fb9
Abhinav <abhinavravi.shankar.s.112@kalvium.community>
2026-03-09 11:39:08 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,48 @@
 import java.util.Scanner;
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
+
         while (true) {
             System.out.print("$ ");
             String input = sc.nextLine();
-            if (input.equals("exit")){
+
+            if (input.equals("exit")) {
                 break;
+
             } else if (input.startsWith("echo ")) {
                 System.out.println(input.substring(5));
+
             } else if (input.startsWith("type ")) {
+
                 String command = input.substring(5);
-                if (command.equals("echo") || command.equals("exit") || command.equals("type")){
+
+                if (command.equals("echo") || command.equals("exit") || command.equals("type")) {
                     System.out.println(command + " is a shell builtin");
                 } else {
-                    System.out.println(command + ": not found");
+
+                    String pathEnv = System.getenv("PATH");
+                    String[] paths = pathEnv.split(File.pathSeparator);
+
+                    boolean found = false;
+
+                    for (String dir : paths) {
+                        File file = new File(dir, command);
+
+                        if (file.exists() && file.canExecute()) {
+                            System.out.println(command + " is " + file.getAbsolutePath());
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (!found) {
+                        System.out.println(command + ": not found");
+                    }
                 }
+
             } else {
                 System.out.println(input + ": command not found");
             }
