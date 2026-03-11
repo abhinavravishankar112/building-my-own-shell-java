18a80f18798370e8c4185ceb19668ff47a85f503
Abhinav <abhinavravi.shankar.s.112@kalvium.community>
2026-03-11 09:02:12 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,13 @@ public class Main {
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
 
-            if (c == '\'' && !inDouble) {
+            if (c == '\\' && !inSingle && !inDouble) {
+                if (i + 1 < input.length()) {
+                    current.append(input.charAt(i + 1));
+                    i++;
+                }
+            }
+            else if (c == '\'' && !inDouble) {
                 inSingle = !inSingle;
             }
             else if (c == '"' && !inSingle) {
